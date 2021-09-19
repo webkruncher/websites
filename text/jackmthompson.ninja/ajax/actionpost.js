44f338a2d771860434334b97bba8d37b0af94bb6
Jack M. Thompson <WebKruncher@GMail.com>
2021-09-19 18:22:06 -0400

    Web forms, ajax web still needs work

--- text/jackmthompson.ninja/ajax/actionpost.js
+++ text/jackmthompson.ninja/ajax/actionpost.js
@@ -4,7 +4,8 @@ function postscrubber()
 	this.scrub=function(who,what)
 	{
 		//if ( buggness ) UserMessages.Write( what.nodeName )
-		if(what.nodeName=="INPUT") this.addnode(who,what)
+		if(what.nodeName=="INPUT") 
+			this.addnode(who,what)
 		for (this.i=0;this.i<what.childNodes.length;this.i++)
 		{
 			this.ps=new postscrubber();
@@ -14,41 +15,42 @@ function postscrubber()
 
 	this.addnode=function(who,what)
 	{
-		who.what+="<"+what.name+"><![CDATA["+what.value+"]]></"+what.name+">\n";
+		who.what+="<"+what.name+" input=\"true\" ><![CDATA["+what.value+"]]></"+what.name+">\n";
 	}
 }
 
 
 
 
-function PostThisForm(how,form,nest, callback, target)
+function PostThisForm(where, how,form,nest, callback, target)
 {
 	this.parts=nest.split("/");
-	this.what="<form>\n"
+	this.what="<" + where + ">\n"
 	for (this.n=0;this.n<this.parts.length;this.n++)
 		this.what+="<"+this.parts[this.n]+">\n"
 	this.ps=new postscrubber();
 	this.ps.scrub(this,form);
 	for (this.n=this.parts.length-1;this.n>=0;this.n--)
 		this.what+="</"+this.parts[this.n]+">\n"
-	this.what+="\n</form>"
+	this.what+="\n</" + where + ">"
 	new ajax( how, target,  callback, null, this.what )
 	return false;
 }
 
-function PostedForm( status )
+function PostedForm( action, status )
 {
-	if ( buggness ) UserMessages.Write( "Status: " + parseInt( status ) )
+	if ( buggness ) UserMessages.Write( action + ": " + parseInt( status ) )
 }
 
-function PostForm( Page, Action, form )
+function PostForm( where, Page, Action, form )
 {
 	f=GetObject( form )
 	target=Navigate( Page, "", "", "", "", "Reset" )
 	target.setAttribute('style','display:block;')
 	target.innerHTML=""
-	PostThisForm( "/" + Page + ".xml", f, Action, "PostedForm( reqnode.httpRequest.status )", target )
+	PostThisForm( where, "/" + Page + ".xml", f, Action, "PostedForm( \"" + Action + "\", reqnode.httpRequest.status )", target )
 	return false
 }
+
 
 
